a62ce3ca3de1675c4a8235fb30f68f0bf5f211d1
cutemiao <dy_zxp@163.com>
2022-05-04 01:40:59 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # haoakanUrl
 ## 功能
 - 获取 [好看视频](https://haokan.baidu.com/) 搜索出来的视频链接
-- 文件保存在当前的目录下的 urlVideo.txt 里面
+- 文件保存在当前的目录下的 URLs 里面，以追加保存
 
 # 注意
 > 使用多了好看视频会阻止访问
--- getUrl.py
+++ getUrl.py
@@ -1,3 +1,4 @@
+import csv
 import re
 
 from bs4 import BeautifulSoup
@@ -20,19 +21,20 @@ def videoUrl(list_url: list) -> list:
 
 def selectVideoSearch(search: str, num: int) -> list:
     list_src = []
-
+    list_title = []
     query = {'query': search, 'rn': 10, 'type': 'video', 'pn': 1}
     for i in range(1, num + 1):
         print(i)
         query['pn'] = i
         r = requests.get("https://haokan.baidu.com/web/search/api?", params=query)
         data = r.json()
+        # print(data)
         data_list = data['data']['list']
         for src in data_list:
             video_url = src['url']
             list_src.append(video_url)
-
-    return list_src
+            list_title.append(src['title'])
+    return list_src, list_title
 
 
 if __name__ == "__main__":
@@ -40,9 +42,11 @@ if __name__ == "__main__":
     search = input('请输入想查找的视频：')
     num = int(input('请输入需要多少的视频链接(10 * )'))
     print("-L- 只能获取大概的数量")
-    list_url = selectVideoSearch(search, num)
+    list_url, list_title = selectVideoSearch(search, num)
     video_url = videoUrl(list_url)
+    print(list_title)
     print(len(video_url))
-    f = open('./urlVideo.txt', 'w')
-    f.write(video_url)
-    f.close()
+    with open('./URLs.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(list_title)
+        writer.writerow(video_url)
